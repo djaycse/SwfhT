a01a8f615d0437a80a2499024102e197ac4baaa3
djay.cse <djay.cse@gmail.com>
2026-08-06 01:26:38 +1000

    Fixed clock and other items from top of screen being hidden with white background colour in light mode theme.

--- app/src/main/java/net/qs/inoffice/MainActivity.kt
+++ app/src/main/java/net/qs/inoffice/MainActivity.kt
@@ -3,12 +3,13 @@ package net.qs.inoffice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import net.qs.inoffice.ui.theme.InOfficeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
             InOfficeTheme(dynamicColor = false) {
                 InOfficeApp()
